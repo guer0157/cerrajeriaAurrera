ba67666e2cb93da5adee6644f57725cacac968c5
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-05 16:15:22 -0500

    s
    s

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 let app={
-    mapsd:document.getElementById("mapImg"),
     init: function(){
-    let size = {
-  width: window.innerWidth || document.body.clientWidth,
-  height: window.innerHeight || document.body.clientHeight
-};
-    async function getMap(){
-        let mapSource=`https://maps.googleapis.com/maps/api/staticmap?center=18.922735%2c%20-99.210363&zoom=18&size=${size.width}x${size.height}&maptype=roadmap&markers=color:red%7Clabel:C%7C18.922735%2c%20-99.210363&key=AIzaSyA0owNrzFrFeVPpDJrl42xKgbs7rq2eIfU&signature=mgDUQaEMTSahgfwGWXANy_4cpQQ=`;
-console.log(mapSource);
-    }
-    await 
+        console.log("hello")
+       let overlaydiv=document.createElement("div");
+        overlaydiv.setAttribute("id","overlayers");
+        let doc=document.querySelector("body");
+        let oferta=document.createElement("div");
+        oferta.setAttribute("id","oferta")
+        let h1=document.createElement("h1");
+        let h2=document.createElement("h2");
+        let h12=document.createElement("h1");
+        let img=document.createElement("img");
+        img.setAttribute("id","ofert")
+        img.src="../img/oferta-8.png"
+        h1.textContent="Oferta!";
+        h2.textContent="Por lanzamiento de nuestra pagina web.";
+        h12.textContent="20% de descuento en servicios a domicilio!";
+        oferta.appendChild(h1);
+        oferta.appendChild(h2);
+        oferta.appendChild(img);
+         oferta.appendChild(h12);
+        overlaydiv.appendChild(oferta)
+        doc.appendChild(overlaydiv);
+        setTimeout(function(){  
+            console.log("something")
+            console.log(doc),
+            doc.removeChild(overlaydiv);
+        },5000)
 }
-/*
-https://maps.googleapis.com/maps/api/staticmap?center=18.922735%2c%20-99.210363&zoom=18&size=1249x214&maptype=roadmap&markers=color:red%7Clabel:C%7C18.922735%2c%20-99.210363&key=AIzaSyA0owNrzFrFeVPpDJrl42xKgbs7rq2eIfU&signature=mgDUQaEMTSahgfwGWXANy_4cpQQ=
-*/
 }
-document.addEventListener('DOMContentLoaded', app.init)
+document.addEventListener('DOMContentLoaded', app.init);
